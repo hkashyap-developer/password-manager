bed28e8776e6fe623e1760535953ed4e570d8a62
Himanshu Kashyap <87484630+hkashyap-developer@users.noreply.github.com>
2024-06-18 21:00:41 +0530

    Changes
    Changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,21 @@ import Footer from '@/components/Footer/Footer'
 import Dashboard from '@/components/Dashboard/Dashboard'
 import CurrentTask from '@/components/CurrentTask/CurrentTask'
 
+
+import { Outfit } from 'next/font/google'
+ 
+const outfit = Outfit({
+  weight: '900',
+  subsets: ['latin'],
+  display: 'swap',
+})
+
+
+
 export default function Home() {
   return (
     <main className="flex flex-col justify-center items-center align-center">
+      <h1 className={outfit.className}>testing</h1>
       <Header/>
       <CurrentTask/>      
       <Dashboard/>
